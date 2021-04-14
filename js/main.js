e900c799db7b685029ac1a4f5da83cf6991d2a0a
scorpio-95 <68906024+scorpio-95@users.noreply.github.com>
2021-04-15 03:19:05 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// function text() {
+// document.getElementById("skilldisplay").style.transitionDelay = "all 2s";
+// document.getElementById("skilldisplay").style.display = "block";
+//
+// }
+//
+// function withouttext() {
+//   document.getElementById("skilldisplay").style.display = "none";
+//
+// }
